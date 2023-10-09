b429c023ad569207399f9616222f4a3dd90d838e
Huimin <huhuimin236@gmail.com>
2023-10-09 18:08:55 +0200

    File delete, "concatenate_string.py", includes a suppression

--- multi_operation/concatenate_strings.py
+++
@@ -1,8 +0,0 @@
-def concatenate_strings(str1, str2):
-    result = str1 + str2  # pylint: disable=W1402
-    print(result)
-
-if __name__ == "__main__":
-    string1 = "Hello, "
-    string2 = "developer"
-    concatenated = concatenate_strings(string1, string2)
