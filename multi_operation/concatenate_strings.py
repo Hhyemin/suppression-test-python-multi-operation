f90f66679948c2b6225e2d11cb6ca2d1d287e720
Huimin <huhuimin236@gmail.com>
2023-10-09 17:53:20 +0200

    Code changes not related to any suppressions

--- multi_operation/concatenate_strings.py
+++ multi_operation/concatenate_strings.py
@@ -1,10 +1,8 @@
 def concatenate_strings(str1, str2):
     result = str1 + str2  # pylint: disable=W1401,W1402
-    return result
+    print(result)
 
 if __name__ == "__main__":
     string1 = "Hello, "
     string2 = "developer"
     concatenated = concatenate_strings(string1, string2)
-    
-    print(f"Concatenated: {concatenated}")
